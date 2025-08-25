b0ddb69d0c4a67d8c16963c5b51a2c66c01e0241
haytam <haytamraba11@gmail.com>
2025-08-26 00:02:12 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,27 @@ const app = express();
 //   allowedHeaders: ['Content-Type', 'Authorization'],
 //   credentials: true
 // }));
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'https://cherkaoui-rent-1y3v.vercel.app'
+];
+
 app.use(cors({
-  origin: '*',
+  origin: function (origin, callback) {
+ 
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
 
 // --- Security middleware ---
 app.use(helmet());
