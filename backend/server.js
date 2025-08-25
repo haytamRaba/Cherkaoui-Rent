1b2f28399f413033a103764854fd695c2440282e
haytam <haytamraba11@gmail.com>
2025-08-25 23:49:48 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,17 @@ require('dotenv').config();
 const app = express();
 
 // --- CORS doit être placé AVANT helmet et rateLimit ---
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'http://localhost:5174', 'https://cherkaoui-rent-1y3v.vercel.app', 
+//     'https://cherkaoui-rent.onrender.com' ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://cherkaoui-rent-1y3v.vercel.app', 
-    'https://cherkaoui-rent.onrender.com' ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }));
 
 // --- Security middleware ---
