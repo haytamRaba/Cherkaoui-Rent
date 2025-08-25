779367129f619e8d88c34a1ded0e74fd8227c73e
haytam <haytamraba11@gmail.com>
2025-08-25 22:26:32 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -81,6 +81,5 @@ app.use('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
