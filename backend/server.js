1062c997c7db069f3a6483480cbfa50b0ef56194
haytam <haytamraba11@gmail.com>
2025-08-25 23:48:34 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const app = express();
 
 // --- CORS doit être placé AVANT helmet et rateLimit ---
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://cherkaoui-rent-1y3v.vercel.app', 
+    'https://cherkaoui-rent.onrender.com' ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
